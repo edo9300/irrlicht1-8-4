050a08f2203dbe84b5c97d59ea2eec2cf548ca80
Edoardo Lolletti <edoardo762@gmail.com>
2021-05-28 23:31:08 +0200

    Fix screenshot in opengl es drivers
    Colors were swapped and there was alpha enabled

--- source/Irrlicht/COGLES2Driver.cpp
+++ source/Irrlicht/COGLES2Driver.cpp
@@ -2762,6 +2762,24 @@ COGLES2Driver::~COGLES2Driver()
 			p2 -= pitch;
 		}
 		delete [] tmpBuffer;
+		
+		// also GL_RGBA doesn't match the internal encoding of the image (which is BGRA)
+		if (GL_RGBA == internalformat && GL_UNSIGNED_BYTE == type)
+		{
+			pixels = static_cast<u8*>(newImage->getData());
+			for (u32 i = 0; i < ScreenSize.Height; i++)
+			{
+				for (u32 j = 0; j < ScreenSize.Width; j++)
+				{
+					u8* output = pixels + 4 * j;
+					u8 tmp = output[0];
+					output[0] = output[2];
+					output[2] = tmp;
+					output[3] = 0xff; //also remove the alpha to not make the screenshot transparent
+				}
+				pixels += pitch;
+			}
+		}
 
 		if (testGLError(__LINE__))
 		{
